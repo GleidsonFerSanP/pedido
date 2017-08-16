11b0a08bd58257ee69113bcfe69a3580fc9abfcd
Gleidson FerSanP <gleidson.ferreirasantos@gmail.com>
2017-08-16 13:20:59 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MyCustomAngularMaterialModule } from './my-custom-angular-material/my-custom-angular-material.module';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MyCustomAngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
